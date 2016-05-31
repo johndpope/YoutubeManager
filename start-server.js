7d57d0887e958592ed374e5c7b44ef3b61ed633a
rdrk <genericor@outlook.com>
2016-05-31 16:44:58 -0300

    change on http file server

--- start-server.js
+++ start-server.js
@@ -1,74 +1,29 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-
-// Create a server
-http.createServer( function (request, response) {  
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
-   
-   // Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
-   if(pathname === '/'){
-    pathname = '/index.html';
-   }
-   pathname = '/build' + pathname;
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else{	
-         //Page found	  
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         var type = {}
-         if(pathname.endsWith('.css')){
-          type['Content-Type'] = 'text/css'
-         }
-         if(pathname.endsWith('.woff')){
-          type['Content-Type'] = 'application/font-woff';
-          type['Access-Control-Allow-Origin'] = '*';
-          console.log(data);
-         }
-         if(pathname.endsWith('.woff2')){
-          type['Content-Type'] = 'application/font-woff2';
-         }
-         if(pathname.endsWith('.ttf')){
-          type['Content-Type'] = 'application/font-ttf';
-         }
-
-         response.writeHead(200, type);	
-
-         
-         // Write the content of the file to response body
-         response.write(data);		
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
-
-
-
 /*
-  Fim http server
+  Inicio http server
 */
 
 var express = require('express');
 var app = express();
 
+app.use(express.static('build'));
+
+app.listen(8081, function(){
+  console.log('server running at http://localhost:8081');
+})
+
+/*
+  Fim http server
+*/
+
+var app2 = express();
+var fs = require('fs');
+
 /*
   post body data
 */
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded({     // to support URL-encoded bodies
+app2.use( bodyParser.json() );
+app2.use( bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
@@ -76,7 +31,7 @@ app.use( bodyParser.urlencoded({     // to support URL-encoded bodies
   end post middleware
 */
 
-app.get('/youtubeExtra', function (req, res) {
+app2.get('/youtubeExtra', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   fs.readFile( __dirname + "/" + "seriesSubscriptions.json", 'utf8', function (err, data) {
@@ -89,7 +44,7 @@ app.get('/youtubeExtra', function (req, res) {
   });
 })
 
-app.post('/youtubeExtra', function(req, res) {
+app2.post('/youtubeExtra', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   fs.readFile( __dirname + "/" + "seriesSubscriptions.json", 'utf8', function (err, data) {
@@ -120,11 +75,11 @@ app.post('/youtubeExtra', function(req, res) {
   });
 });
 
-var server = app.listen(8082, function () {
+var server = app2.listen(8082, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Example app2 listening at http://%s:%s", host, port)
 
 })
