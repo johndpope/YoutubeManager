45655efb41bdf5c738ebb5be586560d67de294dc
rdrk <genericor@outlook.com>
2017-06-28 23:58:22 -0300

    initial structure change

--- source/javascript/structure/Channel.js
+++ source/javascript/structure/Channel.js
@@ -1,10 +1,42 @@
-define(function (){
-	return function Channel( id , name , thumbnail , uploadId , videos, description){
+class Channel {
+	/**
+	 * Creates an instance of Channel.
+	 * @param {string} id 
+	 * @param {string} name 
+	 * @param {string} thumbnail 
+	 * @param {string} description 
+	 * @memberof Channel
+	 */
+	constructor(id, name, thumbnail, description) {
+		var errorMessages = [];
+		if (typeof id !== 'string') {
+			errorMessages.push('Invalid Argument id, expected to be a string.');
+		}
+		if (typeof name !== 'string') {
+			errorMessages.push('Invalid argument name, expected to be a string.');
+		}
+		if (typeof thumbnail !== 'string') {
+			errorMessages.push('Invalid argument thumbnail, expected to be a string.');
+		}
+		if (typeof description !== 'string') {
+			errorMessages.push('Invalid argument description, expected to be a string.');
+		}
+		if(errorMessages.length !== 0) {
+			throw new TypeError(errorMessages.join(' '));
+		}
 		this.id = id;
 		this.name = name;
 		this.thumbnail = thumbnail;
-		this.uploadId = uploadId;
-		this.videos = videos;
 		this.description = description;
+		this.uploadId = 'UU'.concat(id.slice(2));
+		this.videos = [];
 	}
-});	
+}
+
+if( typeof module !== 'undefined') {
+	module.exports = Channel;
+}else {
+	define(function (){
+		return Channel;
+	});	
+}
